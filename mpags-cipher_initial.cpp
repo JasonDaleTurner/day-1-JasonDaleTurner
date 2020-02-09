b6b70aa797c5d59dadc4a894cd7d4735fc5798c1
Jason Turner (Inflammation and Ageing) <turnerjd@bham.ac.uk>
2020-02-09 08:51:30 +0000

    Finished intial version of mpags-cipher_initial.cpp, for the class 1 assignment

--- mpags-cipher_initial.cpp
+++ mpags-cipher_initial.cpp
@@ -3,50 +3,53 @@
 
 int main() {
 	//Create an empty string to store the transliterated input in//
-	std::string input_str{'\0'};
+	std::string input_str{"\0"};
 	
-	//Open while loop to accept input and convert//
-	char in_char{'x'};
+	/*Open while loop to accept input and convert
+	lowercase to upper, check for numerics and
+	convert to strings, and append to the output string
+	to send to stdout*/
+	char in_char{'\0'};
+	std::string zero{"ZERO"};
 	while(std::cin >> in_char){
 		if (isdigit(in_char)) {
 			switch(in_char){
-				case 0:
-					input_str.append("ZERO");
+				case '0':
+					input_str += zero;
 					break;
-				case 1:
-					input_str.append("ONE");
+				case '1':
+					input_str += "ONE";
 					break;
-				case 2:
-					input_str.append("TWO");
+				case '2':
+					input_str += "TWO";
 					break;
-				case 3:
-					input_str.append("THREE");
+				case '3':
+					input_str += "THREE";
 					break;
-				case 4:
-					input_str.append("FOUR");
+				case '4':
+					input_str += "FOUR";
 					break;
-				case 5:
-					input_str.append("FIVE");
+				case '5':
+					input_str += "FIVE";
 					break;
-				case 6:
-					input_str.append("SIX");
+				case '6':
+					input_str += "SIX";
 					break;
-				case 7:
-					input_str.append("SEVEN");
+				case '7':
+					input_str += "SEVEN";
 					break;
-				case 8:
-					input_str.append("EIGHT");
+				case '8':
+					input_str += "EIGHT";
 					break;
-				case 9:
-					input_str.append("NINE");
+				case '9':
+					input_str += "NINE";
 					break;
-		} 
-		else if (isalpha(in_char)){
-			input_str.append(toupper(in_char));
-		} 
+				}			
+			} else if (isalpha(in_char)){
+					input_str += toupper(in_char);
 
-		}
+			}
 	}
-	std::cout << input_str << std::endl;
+std::cout << input_str << std::endl;
 }
 
