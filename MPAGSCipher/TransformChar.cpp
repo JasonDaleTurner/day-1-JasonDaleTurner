96a47d87443b40b46fd837eaf805d42009dcf8ce
Jason Turner (Inflammation and Ageing) <turnerjd@bham.ac.uk>
2020-02-14 15:02:59 +0000

    Began modularising and cmake usage

---
+++ MPAGSCipher/TransformChar.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+//Function definitions//
+std::string transformChar( const char in_char ){
+	//Create an empty string to store the transliterated input in//
+	std::string input_str{"\0"};
+
+	if (isdigit(in_char)) {
+			switch(in_char){
+				case '0':
+					input_str += "ZERO";
+					break;
+				case '1':
+					input_str += "ONE";
+					break;
+				case '2':
+					input_str += "TWO";
+					break;
+				case '3':
+					input_str += "THREE";
+					break;
+				case '4':
+					input_str += "FOUR";
+					break;
+				case '5':
+					input_str += "FIVE";
+					break;
+				case '6':
+					input_str += "SIX";
+					break;
+				case '7':
+					input_str += "SEVEN";
+					break;
+				case '8':
+					input_str += "EIGHT";
+					break;
+				case '9':
+					input_str += "NINE";
+					break;
+				}			
+			} else if (isalpha(in_char)){
+					input_str += toupper(in_char);
+
+			}
+	return input_str;
+}
---
+++ MPAGSCipher/TransformChar.hpp
@@ -0,0 +1,10 @@
+#ifndef MPAGSCIPHER_TRANSFORMCHAR_HPP
+#define MPAGSCIPHER_TRANSFORMCHAR_HPP
+
+#include "TransformChar.hpp"
+#include <string>
+
+//Transliterate char to string//
+std::string transformChar( const char in_char ); 
+
+#endif // MPAGSCIPHER_TRANSFORMCHAR_HPP
