58618d23fc131b29a07b5a861abb012be12713e7
Jason Turner (Inflammation and Ageing) <turnerjd@bham.ac.uk>
2020-02-14 13:18:03 +0000

    Added function for transliteration and began work on arg process function

--- mpags-cipher_initial.cpp
+++ mpags-cipher_initial.cpp
@@ -2,31 +2,14 @@
 #include <string>
 #include <vector>
 
-int main(int argc, char* argv[]) {
-
-	//Printing out command line arguments to test program flexibility//
-	const std::vector<std::string> cmdLineArgs {argv, argv+argc};
-
-	for (size_t i{0}; i < cmdLineArgs.size(); i++){
-
-		std::cout << "Argument: " << cmdLineArgs.at(i)  << std::endl;
-	}
-
-
+std::string transformChar( const char in_char ) {
 	//Create an empty string to store the transliterated input in//
 	std::string input_str{"\0"};
-	
-	/*Open while loop to accept input and convert
-	lowercase to upper, check for numerics and
-	convert to strings, and append to the output string
-	to send to stdout*/
-	char in_char{'\0'};
-	std::string zero{"ZERO"};
-	while(std::cin >> in_char){
-		if (isdigit(in_char)) {
+
+	if (isdigit(in_char)) {
 			switch(in_char){
 				case '0':
-					input_str += zero;
+					input_str += "ZERO";
 					break;
 				case '1':
 					input_str += "ONE";
@@ -60,7 +43,68 @@ int main(int argc, char* argv[]) {
 					input_str += toupper(in_char);
 
 			}
-	}
-std::cout << input_str << std::endl;
+	return input_str;
 }
 
+bool processCommandLine(
+	const std::vector<std::string>& args,
+	bool& helpRequested,
+	bool& versionRequested,
+	std::string& inputFileName,
+	std::string& outputFileName){
+
+}
+
+
+
+
+int main(int argc, char* argv[]) {
+
+	/*Read in the command line arguments and respond appropriately*/
+	const std::vector<std::string> cmdLineArgs {argv, argv+argc};
+
+	std::string input_file{"\0"};
+
+	for (size_t i{0}; i < cmdLineArgs.size(); i++){
+		
+		if (cmdLineArgs.at(i) == "-h"){
+			std::cout << "You need help" << std::endl;
+
+		} else if (cmdLineArgs.at(i) == "--help"){
+			std::cout << "You need help" << std::endl;
+
+		} else if (cmdLineArgs.at(i) == "--version"){
+			std::cout << "mpags-cipher Version 0.1" << std::endl;
+
+		} else if (cmdLineArgs.at(i) == "-i"){
+			if (i+1 > cmdLineArgs.size()-1 ){
+				std::cout << "Input file not provided" << std::endl;
+
+			} else if (cmdLineArgs.at(i+1)[0] == '-'){
+				std::cout << "Input file not provided" << std::endl;
+
+			} else {
+				input_file += cmdLineArgs.at(i+1);
+			}
+		}
+	}
+
+
+	std::cout << "Type text and hit control + D when finished:" << std::endl;
+	
+	/*Open while loop to accept input and convert
+	lowercase to upper, check for numerics and
+	convert to strings, and append to the output string
+	to send to stdout*/
+	std::string output_str{"\0"};
+	char in_char{'\0'};
+	
+	while(std::cin >> in_char){
+		output_str += transformChar(in_char);
+
+
+	}
+std::cout <<  std::endl;
+std::cout << output_str << std::endl;
+
+}
