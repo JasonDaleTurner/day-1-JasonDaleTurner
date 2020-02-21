1e16bc350723f8c836aa8b4b45779c37ead9bf7e
JasonDaleTurner <43812311+JasonDaleTurner@users.noreply.github.com>
2020-02-21 11:55:53 +0000

    Delete TransformChar.hpp

--- MPAGSCipher/TransformChar.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MPAGSCIPHER_TRANSFORMCHAR_HPP
-#define MPAGSCIPHER_TRANSFORMCHAR_HPP
-
-#include <string>
-
-//Transliterate char to string//
-std::string transformChar( const char in_char ); 
-
-#endif // MPAGSCIPHER_TRANSFORMCHAR_HPP
